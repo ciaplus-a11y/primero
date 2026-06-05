b61f61111c407bd041d509f4acfcf06652887cbe
Saul <ciaplus@gmail.com>
2026-06-05 08:49:16 -0600

    Cambios Nuevos

---
+++ prueba.py
@@ -0,0 +1,4 @@
+with open("README.md", "w", encoding="utf-8") as archivo:
+    archivo.write("# Modificado desde Python\n")
+    archivo.write("Este es un cambio real para que git lo detecte\n")
+print("Archivo modificado correctamente")
